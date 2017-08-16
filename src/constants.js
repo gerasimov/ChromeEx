84babfe3e301251a41df9db2210aa5039b3ee158
Руслан Герасимов <ruslan.gerasimov@simbirsoft.com>
2017-08-16 19:06:38 +0400

    Make uniq compile id

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
-export const CONTENT = 0;
-export const INCLUDE = 1;
-export const BACKGROUND = 2;
+const id = process.env.CHROME_EX || "";
+
+export const CONTENT = `content${id}`;
+export const INCLUDE = `include${id}`;
+export const BACKGROUND = `background${id}`;
 
 export const INCLUDE_SEND = `chromex--${INCLUDE}--send-message`;
 export const CONTENT_SEND = `chromex--${CONTENT}--send-message`;
